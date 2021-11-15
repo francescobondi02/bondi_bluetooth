dc6d3558b9fba2973d77085c492a61e10ebdeec3
Bondz <francesco.bondi02@gmail.com>
2021-11-15 20:19:02 +0100

    commit in 'master'

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
-import 'dart:async';
+/*import 'dart:async';*/
 import 'dart:math';
 
-/*import 'package:flutter/material.dart';*/
-/*import 'package:flutter_blue/flutter_blue.dart';*/
 import 'package:flutter/material.dart';
 import 'package:polar/polar.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
-//import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:flutter_plot/flutter_plot.dart';
 
 void main() {
   runApp(MyApp());
@@ -23,12 +20,32 @@ class _MyAppState extends State<MyApp> {
   late final Polar polar;
   List<String> logs = ['Service started'];
 
+//Parte del plot
+  final List<Point> data = [
+    /*const Point(21.0, 19.0),
+    const Point(3.0, 7.0),
+    const Point(8.0, 9.0),
+    const Point(11.0, 14.0),
+    const Point(18.0, 17.0),
+    const Point(7.0, 8.0),
+    const Point(-4.0, -4.0),
+    const Point(6.0, 12.0),*/
+    const Point(0, 0)
+  ];
+
   @override
   void initState() {
     super.initState();
-
+    var time = 0;
     polar = Polar();
-    polar.heartRateStream.listen((e) => log('Heart rate: ${e.data.hr}'));
+    //log(polar);
+    polar.heartRateStream.listen((e) => {
+          //log('Heart rate: ${e.data.hr}');
+          setState(() {
+            data.insert(data.length, Point(time, e.data.hr));
+            time++;
+          })
+        });
     polar.streamingFeaturesReadyStream.listen((e) {
       if (e.features.contains(DeviceStreamingFeature.ecg)) {
         polar
@@ -45,33 +62,119 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Polar App'),
-            actions: [
-              IconButton(
-                icon: Icon(Icons.stop),
-                onPressed: () {
-                  log('Disconnecting from device: $identifier');
-                  polar.disconnectFromDevice(identifier);
-                },
-              ),
-              IconButton(
-                icon: Icon(Icons.play_arrow),
-                onPressed: () {
-                  log('Connecting to device: $identifier');
-                  polar.connectToDevice(identifier);
-                },
-              ),
-            ],
-          ),
-          body: ListView(
+        appBar: AppBar(
+          title: const Text('Polar App - F.Bondi'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.stop),
+              onPressed: () {
+                log('Disconnecting from device: $identifier');
+                polar.disconnectFromDevice(identifier);
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.play_arrow),
+              onPressed: () {
+                log('Connecting to device: $identifier');
+                polar.connectToDevice(identifier);
+              },
+            ),
+          ],
+        ),
+        /*body: ListView(
             padding: EdgeInsets.all(10),
             shrinkWrap: true,
             children: logs.reversed.map((e) => Text(e)).toList(),
-          )),
-      /*body: Scaffold(
-            body: SfCartesianChart(),
           )),*/
+        body: ListView(
+          children: <Widget>[
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.only(top: 12.0),
+                    child: const Text('Heart Rate Plot (no trace)'),
+                  ),
+                  Container(
+                    child: Plot(
+                      height: 200.0,
+                      data: data,
+                      gridSize: Offset(5.0, 10.0),
+                      style: PlotStyle(
+                        pointRadius: 3.0,
+                        outlineRadius: 1.0,
+                        primary: Colors.white,
+                        secondary: Colors.orange,
+                        textStyle: const TextStyle(
+                          fontSize: 8.0,
+                          color: Colors.blueGrey,
+                        ),
+                        axis: Colors.blueGrey[600],
+                        gridline: Colors.blueGrey[100],
+                      ),
+                      padding:
+                          const EdgeInsets.fromLTRB(40.0, 12.0, 12.0, 40.0),
+                      xTitle: 'Time (seconds)',
+                      yTitle: 'BPM',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.only(top: 12.0),
+                    child: const Text(
+                        'Heart Rate Plot (with trace and coordinates)'),
+                  ),
+                  Container(
+                    child: Plot(
+                      height: 200.0,
+                      data: data,
+                      gridSize: Offset(5.0, 10.0),
+                      style: PlotStyle(
+                        axisStrokeWidth: 2.0,
+                        pointRadius: 3.0,
+                        outlineRadius: 1.0,
+                        primary: Colors.yellow,
+                        secondary: Colors.red,
+                        trace: true,
+                        traceStokeWidth: 3.0,
+                        traceColor: Colors.blueGrey,
+                        traceClose: false,
+                        showCoordinates: true,
+                        textStyle: const TextStyle(
+                          fontSize: 8.0,
+                          color: Colors.grey,
+                        ),
+                        axis: Colors.blueGrey[600],
+                        gridline: Colors.blueGrey[100],
+                      ),
+                      padding:
+                          const EdgeInsets.fromLTRB(40.0, 12.0, 12.0, 40.0),
+                      xTitle: 'Time (seconds)',
+                      yTitle: 'BPM',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Text(
+              'Log',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold, fontSize: 25, height: 3),
+              textAlign: TextAlign.center,
+            ),
+            ListView(
+              padding: EdgeInsets.all(10),
+              shrinkWrap: true,
+              children: logs.reversed.map((e) => Text(e)).toList(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
